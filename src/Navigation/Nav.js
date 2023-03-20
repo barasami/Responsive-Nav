cfa9970f3ae65158ff01fdfada44a67bfec0127c
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-03-20 22:40:03 +0300

    added icones

--- src/Navigation/Nav.js
+++ src/Navigation/Nav.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Nav.css'
 import ClearIcon from '@mui/icons-material/Clear';
 import DehazeIcon from '@mui/icons-material/Dehaze';
 
 function Nav() {
+    const[menu,setMenu]=useState(false)
+    const changeMe=()=>{
+        setMenu(!menu)
+    }
   return (
     <nav>
         <div>SimpleNav</div>
-        <div><DehazeIcon/></div>
+        <div onClick={changeMe}>
+            {menu ? <ClearIcon/>: <DehazeIcon/>  }
+        </div>
         <div>
             <ul>
                 <li>home</li>
