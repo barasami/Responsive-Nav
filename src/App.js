5062d73b4a19f4628cd17f602125922ffdf67632
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-03-23 07:10:20 +0300

    resp nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Nav from './Navigation/Nav';
+import Body from './Body/Body';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
+      <Body/>
     </div>
   );
 }
---
+++ src/Body/Body.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Body.css'
+
+function Body() {
+  return (
+    <div>
+      <p className='mybody'>Responsive Navigation bar</p>
+    </div>
+  )
+}
+
+export default Body
